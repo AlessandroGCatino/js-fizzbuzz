3e1dc96145d9cad26f4e739d90d302a022846fde
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-09 16:23:10 +0100

    conclusione esercizio base

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("ciao")
+const mul3 = "Fizz"
+const mul5 = "Buzz"
+
+for(i=1; i<=100; i++)
+{
+    if((i%3 == 0) && (i%5 == 0)){
+        console.log(mul3+mul5)
+    }
+    if((i%3 == 0)){
+        console.log(mul3)
+    } else if ((i%5 == 0)){
+        console.log(mul5)
+    }
+    else {
+        console.log(i)
+    }
+    
+}
